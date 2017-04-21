4df28605207c408a51cbfb6380f629fb31371e9f
Hendrik Liebau <mail@hendrik-liebau.de>
2017-04-21 22:56:45 +0200

    test: add a first unit test (#2)
    * test: add a first unit test
    
    * fix: prevent line break issues in snapshot tests
    
    * ci: do not test on node 4

---
+++ src/__tests__/helper.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs-promise';
+import * as path from 'path';
+
+function getDirectories(dirname: string): string[] {
+  return fs.readdirSync(dirname)
+    .filter(file => fs.statSync(path.join(dirname, file)).isDirectory());
+}
+
+export interface TestModule {
+  name: string;
+  entry: string;
+}
+
+export function getModules(): TestModule[] {
+  const fixturesDirname = path.join(__dirname, '__fixtures__');
+
+  return getDirectories(fixturesDirname).map(moduleName => ({
+    name: moduleName,
+    entry: path.join(fixturesDirname, moduleName, 'index.js')
+  }));
+}
