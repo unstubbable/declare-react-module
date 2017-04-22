fa1da8e308bb050e10c6479cc928e8077ba8cbae
Hendrik Liebau <mail@hendrik-liebau.de>
2017-04-22 14:35:44 +0200

    refactor: add cli.js as bin file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import * as fs from 'fs-promise';
 import * as path from 'path';
 import {generateTypings} from './generate';
@@ -8,14 +6,16 @@ const pkgDir = process.cwd();
 const {name, main} = require(path.join(pkgDir, 'package.json'));
 const entry = path.join(pkgDir, main);
 
-// tslint:disable-next-line no-floating-promises
-generateTypings(name, entry).then(async typings => {
-  const typingsFilename = path.join(pkgDir, 'index.d.ts');
-  return fs.writeFile(typingsFilename, typings);
-}).catch(reason => {
-  console.log(
-    `Error while generating declaration file for ${name}.`,
-    reason.message
-  );
-  process.exit(1);
-});
+export function cli(): void {
+  // tslint:disable-next-line no-floating-promises
+  generateTypings(name, entry).then(async typings => {
+    const typingsFilename = path.join(pkgDir, 'index.d.ts');
+    return fs.writeFile(typingsFilename, typings);
+  }).catch(reason => {
+    console.log(
+      `Error while generating declaration file for ${name}.`,
+      reason.message
+    );
+    process.exit(1);
+  });
+}
