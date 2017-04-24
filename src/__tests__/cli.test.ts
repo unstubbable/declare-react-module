bd178b9b72d7158db249bf1cff8dae3bdadd81e7
Hendrik Liebau <mail@hendrik-liebau.de>
2017-04-25 00:03:53 +0200

    test: dry up repetitive tests

--- src/__tests__/cli.test.ts
+++ src/__tests__/cli.test.ts
@@ -5,11 +5,7 @@ import {cli} from '../';
 describe('cli()', () => {
   let pkgDir: string;
 
-  describe('with an absolute package dir', () => {
-    beforeEach(() => {
-      pkgDir = path.join(__dirname, '__fixtures__', 'export-from-js');
-    });
-
+  const writesDeclarationFile = () => {
     it('writes a declaration file to the given package dir', async () => {
       const filename = path.join(pkgDir, 'index.d.ts');
       await fs.remove(filename);
@@ -19,6 +15,14 @@ describe('cli()', () => {
       const fileContents = await fs.readFile(filename);
       expect(fileContents.toString()).toMatchSnapshot();
     });
+  };
+
+  describe('with an absolute package dir', () => {
+    beforeEach(() => {
+      pkgDir = path.join(__dirname, '__fixtures__', 'export-from-js');
+    });
+
+    writesDeclarationFile();
   });
 
   describe('with a relative package dir', () => {
@@ -26,15 +30,7 @@ describe('cli()', () => {
       pkgDir = path.join('src', '__tests__', '__fixtures__', 'export-from-js');
     });
 
-    it('writes a declaration file to the given package dir', async () => {
-      const filename = path.join(pkgDir, 'index.d.ts');
-      await fs.remove(filename);
-
-      await cli(pkgDir);
-
-      const fileContents = await fs.readFile(filename);
-      expect(fileContents.toString()).toMatchSnapshot();
-    });
+    writesDeclarationFile();
   });
 
   it('throws if no package.json can be found', async () => {
