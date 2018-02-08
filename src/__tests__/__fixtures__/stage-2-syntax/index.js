9dfe8f7d3b58d35eb7e8abb2c73a444d24d6fb9b
Hendrik Liebau <mail@hendrik-liebau.de>
2018-02-08 09:53:37 +0100

    feat: update react2dts to support PureComponent (#38)
    This also updates all other dependencies.

--- src/__tests__/__fixtures__/stage-2-syntax/index.js
+++ src/__tests__/__fixtures__/stage-2-syntax/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
-export class MyComponent extends React.Component {
+export class MyComponent extends React.PureComponent {
   static propTypes = {
     foo: PropTypes.string.isRequired,
   };
